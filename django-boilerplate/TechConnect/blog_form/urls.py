0ef39ea92ef9a27f0bda0e3560f94a28f33ee315
AnanyaM12 <ananyaamathur03@gmail.com>
2023-07-12 00:35:54 +0530

    commit_2 issue#8

--- django-boilerplate/TechConnect/blog_form/urls.py
+++ django-boilerplate/TechConnect/blog_form/urls.py
@@ -4,5 +4,4 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('submit/', views.submit_blog, name='submit_blog'),
-    path('submission-success/', views.submission_success, name='submission_success'),
 ]
