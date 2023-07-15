4e722fe07c225f56f5ed88bc39cc551d91789a32
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2023-07-16 01:34:51 +0530

    Added User-Login Authentication with Outlook under Issue #27
    Used Azure AD to register an application and used that to authenticate and login user in Django.
    
    To use the login feature follow the following steps:
    
    cd to path of your project
    
    python3 -m venv venv // Skip this step if you already have a venv
    
    source venv/bin/activate
    
    pip3 install -r requirements.txt
    python manage.py collectstatic
    python manage.py migrate
    python manage.py runserver localhost:8000
    
    Currently using my own credentials for app registration through Azure AD.
    
    The Application Config can be changed through changing the 'ClientID', 'TenantID', and 'SecretKey' from aad.config.json file.

--- django-boilerplate/TechConnect/TechConnect/urls.py
+++ django-boilerplate/TechConnect/TechConnect/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for TechConnect project.
+"""Sample URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from ms_identity_web.django.msal_views_and_urls import MsalViews
+
+msal_urls = MsalViews(settings.MS_IDENTITY_WEB).url_patterns()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog_form.urls', namespace='blog'))
+    path('', views.index, name='index'),
+    path('sign_in_status', views.index, name='status'),
+    path('token_details', views.token_details, name='token_details'),
+    path('blog/', include('blog_form.urls', namespace='blog')),
+    path(f'{settings.AAD_CONFIG.django.auth_endpoints.prefix}/', include(msal_urls)),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
